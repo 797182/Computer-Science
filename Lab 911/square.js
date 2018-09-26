8625e2dff7878db721be5da972ad6ed3d13a67c6
797182 <43476796+797182@users.noreply.github.com>
2018-09-25 23:08:01 -0700

    Update square.js

--- Lab 911/square.js
+++ Lab 911/square.js
@@ -1,29 +1,20 @@
-var squares = [];
-Var numberOfSquares = 8;
-Var squareSize;
+function Sqr(location, sidelengh, color){
+//  Add instance variables to your constructor Function
+//What does a square need to know about itself??
+this.loc=location;
+this.w=sidelengh;
+this.col=color;
 
-function setup () {
-	var cnv = createCanvas(800, 800);
-	cnv.position((windowWidth-width)/2, 30);
-	squareSize = width/numberofSquares;
-	loadSquares(8);
-
-	for (var i = 0; i < squares.length; i++) {
-		squares[i].render();
-	}
+this.run = function(){
+  this.checkEdges();
+  this.update();
+  this.render();
 }
 
-function loadSquares() {
-	var rowNum = 0;
-	var colNum = 0;
+  this.render = function(){
+    fill(this.col);
+    rect(this.loc.x, this.loc.y, this.w, this.w);
 
-	for (var row = 0; row < 8; row++) {
-		var x = 100*row;
-for (var col = 0; col < 8; col++) {
-	var y = 100*col;
-	var loc = createVector(x, y);
-	var c;
-	if (x==0)
-}
-	}
-}
+  }
+
+}//  End of Square constructor function
